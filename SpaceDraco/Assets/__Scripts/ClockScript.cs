1d6470c77cc4f3acfc828d854f36bc77203cb068
Created unity Project <nrhymer@students.kennesaw.edu>
2020-02-03 23:27:16 -0500

    SpaceDraco - Full Game Upload
    Variation Of Space Shooter Survival game created in 2016

---
+++ SpaceDraco/Assets/__Scripts/ClockScript.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+
+public class ClockScript : MonoBehaviour
+{
+	public int level = 1;
+	public int timeLeft = 60;
+	public Text clockText;
+	public Text levelText;
+
+	// Use this for initialization
+	void Start()
+	{
+		StartCoroutine("LoseTime");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		clockText.text = ("Level up in = " + timeLeft);
+
+		if (timeLeft <= 0)
+		{
+			StopCoroutine("LoseTime");
+			timeLeft = 60;
+			level = level + 1;
+			levelText.text = ("Level: ") + level;
+			StartCoroutine("LoseTime");
+
+		}
+	}
+
+	IEnumerator LoseTime()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(1);
+			timeLeft--;
+		}
+	}
+}
+
+
+
+
+
+
+ /* public class ClockScript : MonoBehaviour {
+
+	public int level = 1;
+	public int startTime = 60;
+	public Text clockText;
+	public Text levelText;
+	private int timeRemaining;
+	static public bool timesUp = false;
+	public Text gameoverText;
+
+	// Use this for initialization
+	void Start () {
+		clockText.text = "Level up in = 0:00";
+
+	}
+	// Update is called once per frame
+	void Update () {
+
+		DoCountDown ();
+
+	
+	}
+
+	void DoCountDown()
+	{
+		
+
+
+
+		timeRemaining = startTime - (int)Time.time;
+
+		do 
+		if (timeRemaining <= 0 || timesUp == true) {
+
+			Reset ();
+
+		} 
+		else 
+		{
+			int minutes = timeRemaining / 60;
+			int seconds = timeRemaining % 60;
+			clockText.text = "Level Up In = " + minutes + ":" +
+			seconds.ToString ("D2");
+		}
+		while (level >=100);
+	}
+
+		void Reset()
+	{
+		startTime = 70;
+		timesUp = false;
+		level = level + 1;
+	}
+	
+
+	} */
+
---
+++ SpaceDraco/Assets/__Scripts/PauseGame.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour {
+
+	public Transform canvas;
+	public Text gameoverText; 
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) 
+		{
+			Pause();
+		}
+		if (Input.GetKeyDown (KeyCode.Q)) 
+		{
+			Quit ();
+		}
+		if (Input.GetKeyDown (KeyCode.R)) 
+		{
+			Restart ();
+		}
+		if (Input.GetKeyDown (KeyCode.P))
+		{
+			canvas.gameObject.SetActive (false);
+			Time.timeScale = 1;
+		}
+	}
+
+	public void Pause()
+	{
+		
+			if (canvas.gameObject.activeInHierarchy == false)
+			{
+				canvas.gameObject.SetActive (true);
+				Time.timeScale = 0;
+			} 
+			else
+			{
+				canvas.gameObject.SetActive (false);
+				Time.timeScale = 1;
+			}
+		}
+	public void Restart()
+	{
+		
+
+		Application.LoadLevel ("_Scene_0");
+		//ClockScript.startTime = 60;
+		Time.timeScale = 1;
+		//ClockScript.timesUp = false;
+}
+
+	public void Quit()
+	{
+		canvas.gameObject.SetActive (false);
+		gameoverText.text = "Game O'V";
+		Time.timeScale = 0;
+	}
+
+}
